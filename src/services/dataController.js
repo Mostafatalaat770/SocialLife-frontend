b9060249ab875b51d60188c95d4df91714b00784
Mostafa Talaat <mostafatalaat770@gmail.com>
2020-05-11 00:27:37 +0200

    dataController: updated url and removed a unused method
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/services/dataController.js
+++ src/services/dataController.js
@@ -1,15 +1,12 @@
 import axios from 'axios'
 
-const url = "https://localhost:3001"
+const url = "http://localhost:3001"
 
 const login = (input) => {
-    return axios.post(url, input).then(response => response)
+    return axios.post(`${url}/login`, input).then(response => response)
 }
 const signup = (input) => {
     return axios.post(`${url}/signup`, input).then(response => response)
-}
-const getCurrentProfile = () => {
-   return axios.get(`${url}/profile`).then(response => response)
 }
 const updateCurrentProfile = (updatedData) => {
    return axios.put(`${url}/profile/edit`, updatedData).then(response => response)
