641fa7a89411e2823defcfed72a6a2ee0a591d0f
Mostafa Talaat <mostafatalaat770@gmail.com>
2020-05-11 00:29:17 +0200

    dataController: added new methods
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/services/dataController.js
+++ src/services/dataController.js
@@ -11,10 +11,40 @@ const signup = (input) => {
 const updateCurrentProfile = (updatedData) => {
    return axios.put(`${url}/profile/edit`, updatedData).then(response => response)
 }
-
+const getUserByID = (ID) => {
+    return axios.get(`${url}/user/${ID}`).then(response => response)
+}
+const addPublicPost = (post) => {
+    return axios.post(`${url}/posts/public`, post).then(response => response)
+}
+const addPrivatePost = (post) => {
+    return axios.post(`${url}/posts/private`, post).then(response => response)
+}
+const acceptFriendRequest = (FriendID) => {
+    return axios.post(`${url}/user/${FriendID}/acceptFriendRequest`).then(response => response)
+}
+const sendFriendRequest = (FriendID) => {
+    return axios.post(`${url}/user/${FriendID}/sendFriendRequest`).then(response => response)
+}
+const deleteFriendRequest = (FriendID) => {
+    return axios.delete(`${url}/user/${FriendID}/deleteFriendRequest`).then(response => response)
+}
+const unfriend = (FriendID) => {
+    return axios.delete(`${url}/user/${FriendID}/unfriend`).then(response => response)
+}
+const getFriendsPosts = () => {
+    return axios.get(`${url}/homepage`).then(response => response)
+}
 export {
     login,
     signup,
-    getCurrentProfile,
-    updateCurrentProfile
+    updateCurrentProfile,
+    getUserByID,
+    addPublicPost,
+    addPrivatePost,
+    acceptFriendRequest,
+    sendFriendRequest,
+    deleteFriendRequest,
+    unfriend,
+    getFriendsPosts
 }
