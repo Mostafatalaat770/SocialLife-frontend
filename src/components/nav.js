964e727b2d700a22230dc031da1d92de0ea61d22
Mostafa Talaat <mostafatalaat770@gmail.com>
2020-05-17 01:08:28 +0200

    implemented nav component
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

---
+++ src/components/nav.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import "../../public/css/bootstrap/bootstrap.min.css";
+const Nav = ({ currentUser, setCurrentPage, setRequestedID }) => {
+	const [searchIndicator, setsearchIndicator] = useState("Name");
+	const [searchMode, setSearchMode] = useState(0);
+
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<span className="navbar-brand mb-0 h1">Social Life</span>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div
+				className="collapse navbar-collapse"
+				id="navbarSupportedContent"
+			>
+				<ul className="navbar-nav mr-auto">
+					<li className="nav-item active">
+						<a
+							className="nav-link"
+							href="#"
+							onClick={() => {
+								setCurrentPage("homePage");
+							}}
+						>
+							Home <span className="sr-only">(current)</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a
+							className="nav-link"
+							href="#"
+							onClick={() => {
+                                setCurrentPage("profilePage");
+                                setRequestedID(currentUser.ID)
+							}}
+						>
+							Profile
+						</a>
+					</li>
+
+					<li className="nav-item">
+						<a className="nav-link" href="#"
+                        onClick={() => {
+                            setCurrentPage("friendsPage");
+                        }}>
+							Friends
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#"
+                        onClick={() => {
+                            setCurrentPage("friendRequestsPage");
+                        }}>
+							Friends Requests
+						</a>
+					</li>
+				</ul>
+				<form className="form-inline my-2 my-lg-0">
+					<input
+						className="form-control mr-sm-2"
+						type="search"
+						placeholder="Search"
+						aria-label="Search"
+					/>
+					<div className="dropdown">
+						<button
+							className="btn btn-secondary dropdown-toggle"
+							type="button"
+							id="dropdownMenuButton"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false"
+						>
+							{searchIndicator}
+						</button>
+						<div
+							className="dropdown-menu"
+							aria-labelledby="dropdownMenuButton"
+						>
+							<a
+								className="dropdown-item"
+								href="#"
+								onClick={() => {
+									setsearchIndicator("Name");
+									setSearchMode(0);
+								}}
+							>
+								Name
+							</a>
+							<a
+								className="dropdown-item"
+								href="#"
+								onClick={() => {
+									setsearchIndicator("Email");
+									setSearchMode(1);
+								}}
+							>
+								Email
+							</a>
+							<a
+								className="dropdown-item"
+								href="#"
+								onClick={() => {
+									setsearchIndicator("Phone Number");
+									setSearchMode(2);
+								}}
+							>
+								Phone Number
+							</a>
+							<a
+								className="dropdown-item"
+								href="#"
+								onClick={() => {
+									setsearchIndicator("Hometown");
+									setSearchMode(3);
+								}}
+							>
+								Hometown
+							</a>
+							<a
+								className="dropdown-item"
+								href="#"
+								onClick={() => {
+									setsearchIndicator("Caption");
+									setSearchMode(4);
+								}}
+							>
+								Caption
+							</a>
+						</div>
+					</div>
+
+					<button
+						className="btn btn-outline-success my-2 my-sm-0"
+						type="submit"
+					>
+						Search
+					</button>
+				</form>
+			</div>
+		</nav>
+	);
+};
+export default Nav;
