eaed3b397c727d34a9a1f19c3d6d2e3a8aa58a81
Mostafa Talaat <mostafatalaat770@gmail.com>
2020-05-17 01:42:02 +0200

    index: added new components added navbar for alll current pages, added friends and friendRequests pages
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import Login from "./components/login"
 import Signup from "./components/signup"
 import Profile from "./components/profile"
-
+import Friends from "./components/friends"
+import FriendRequests from "./components/friendRequests"
+import Nav from "./components/nav"
 
 const App = () => {
 	const [currentUser, setCurrentUser] = useState(null);
 	const [currentPage, setCurrentPage] = useState("login");
+	const [requestedID, setRequestedID] = useState(null)
 	if (currentPage === "login") {
 		return (
 			<div>
@@ -31,8 +34,25 @@ const App = () => {
 
 	if(currentPage === "profilePage"){
 		return <>
-			<Profile requestedID={currentUser.ID} />
+		<Nav currentUser={currentUser} setCurrentPage={setCurrentPage} setRequestedID={setRequestedID} />
+			<Profile requestedID={requestedID} currentUserID={currentUser.ID}/>
 		</>
+	}
+	if(currentPage === "friendsPage"){
+		return (
+		<>
+		<Nav currentUser={currentUser} setCurrentPage={setCurrentPage} setRequestedID={setRequestedID} />
+		<Friends setRequestedID={setRequestedID} setCurrentPage={setCurrentPage} />
+		</>
+		)
+	}
+	if(currentPage === "friendRequestsPage"){
+		return (
+			<>
+			<Nav currentUser={currentUser} setCurrentPage={setCurrentPage} setRequestedID={setRequestedID} />
+			<FriendRequests setRequestedID={setRequestedID} setCurrentPage={setCurrentPage} />
+			</>
+			)
 	}
 	return <div></div>;
 };
