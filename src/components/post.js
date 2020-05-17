46fe7dd08d4a2b3ae73378b1427ecbdbc32d277d
Mostafa Talaat <mostafatalaat770@gmail.com>
2020-05-17 22:47:06 +0200

    post: added style to posts and fixed images
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/components/post.js
+++ src/components/post.js
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { getUserInfoByID } from "../services/dataController";
 
-const PosterInfo = ({posterID}) => {
+const PosterInfo = ({posterID, postTime, setCurrentPage, setRequestedID}) => {
 	const [posterInfo, setPosterInfo] = useState(null)
-	useEffect(() => {getUserInfoByID(posterID).then(response => setPosterInfo(response.data))}, [])
+	useEffect(() => {getUserInfoByID(posterID).then(response => {setPosterInfo(response.data)
+	console.log(response.data);})}, [])
 	
 
 	if(!posterInfo){
 		return <div>loading...</div>
 	}
+	const handleFriendClick = (ID) => {
+        setCurrentPage("profilePage")
+        setRequestedID(ID)
+    }
 
 	return(
-		<div>
-			<img src={posterInfo.profile_picture} style={{float: "left"}}/>
-			<div>{`${posterInfo.Fname} ${posterInfo.Lname}`}</div>
-		</div>
+			<div className="media" style={{marginRight: 30}}>
+  <img src={`/images/profilePictures/${posterInfo.profile_picture}`} className="align-self-start mr-3" alt="..."/>
+  <div className="media-body">
+    <a href="#" onClick={() => handleFriendClick(posterID)} className="mt-0">{`${posterInfo.Fname} ${posterInfo.Lname}`}</a>
+    <p>{postTime}</p>
+  </div>
+</div>
 	)
 }
 
-const Post = ({post}) => {
+const Post = ({post, setCurrentPage, setRequestedID}) => {
 	return(
-		<div>
-			 <PosterInfo posterID={post.posted_by} />
-			 <span>{post.time}</span>
-			<p>{post.text_content}</p>
-			<img src={post.image_content}/>
+		<div style={{display: "inline-flex"}}>
+			 <PosterInfo posterID={post.posted_by} postTime={post.time} setCurrentPage={setCurrentPage} setRequestedID={setRequestedID}/>			 
+  <div className="card" style={{ width: "18rem" }}>
+  <img src="/images/postPictures/test.jpg" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <p className="card-text">
+	{post.text_content}
+    </p>
+  </div>
+</div>
 			</div>
 	)
 }
